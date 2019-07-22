1c78d33601ce33a4fca0977bd618776e24160072
Igor Loboda <igore4ek061@gmail.com>
2019-07-22 16:04:13 +0300

    Home Work 2

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require "src/functions.php";
+
+task1(["str1","str2","str3","str4"], true);
+echo '<br>';
+task2("+", 1, 2, 3);
+echo '<br>';
+task2("-", 1, 1, 1);
+echo '<br>';
+task2("/", 8, 2, 2);
+echo '<br>';
+task2("*", 3, 3, 3);
+echo '<br>';
+task3(8,8);
+task4();
+echo '<br>';
+task5("24.02.2016 00:00:00");
+echo '<br>';
+task6("Карл у Клары украл Кораллы");
+echo '<br>';
+task7("Две бутылки лимонада");
+
+$f = fopen("test.txt", "wb");
+fwrite($f, "Hello Again!");
+fclose($f);
+
+task8("test.txt");
---
+++ src/functions.php
@@ -0,0 +1,133 @@
+<?php
+
+function task1($arr, $flag = false)
+{
+    if ($flag) {
+        return implode("", $arr);
+    } else {
+        for ($i = 0; $i < sizeof($arr); $i++) {
+            echo "<p>" . $arr[$i] . "</p>";
+        }
+    }
+}
+
+function task2($act, ...$numbers)
+{
+    if (count($numbers) > 1) {
+        switch ($act) {
+            case '+':
+                echo add(...$numbers);
+                break;
+            case '-':
+                echo subtract(...$numbers);
+                break;
+            case '/':
+                echo divide(...$numbers);
+                break;
+            case '*':
+                echo multiply(...$numbers);
+                break;
+            default:
+                echo "The action does not exist";
+                break;
+        }
+
+    } else {
+        return $numbers[0];
+    }
+}
+
+function task3($start, $end)
+{
+    echo '<table border="1">';
+
+    for ($i = 1; $i <= $start; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= $end; $j++) {
+            echo "<td>${j} x ${i} = " . $j * $i . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo '</table>';
+
+}
+
+function task4()
+{
+    echo date('d.m.Y H:i');
+}
+
+function task5($str)
+{
+    if (($timestamp = strtotime($str)) === false) {
+        echo "Строка ($str) недопустима";
+    } else {
+        echo $timestamp;
+    }
+}
+
+function task6($str)
+{
+    echo str_replace("К", "", $str);
+}
+
+function task7($str)
+{
+    echo str_replace("Две", "Три", $str);
+}
+
+function task8($file)
+{
+    $content = "";
+    if (file_exists($file)) {
+        $f = fopen($file, "rb");
+        $content = fread($f, filesize(($file)));
+        fclose($f);
+    } else {
+         echo "File does not exist!";
+    }
+
+    echo $content;
+
+}
+
+/* Helper functions for task3 */
+function subtract(...$numbers)
+{
+    $n = $numbers[0];
+    for ($i = 0; $i < count($numbers); $i++) {
+        $n -= $numbers[$i+1];
+    }
+    return $n;
+}
+
+function add(...$numbers)
+{
+    $result = 0;
+    foreach ($numbers as $num) {
+        $result += $num;
+    }
+    return $result;
+}
+
+function divide(...$numbers)
+{
+    $n = $numbers[0];
+    for ($i = 0; $i < count($numbers); $i++) {
+        if (isset($numbers[$i+1])) {
+            $n = $n / $numbers[$i+1];
+        }
+    }
+    return $n;
+}
+
+function multiply(...$numbers)
+{
+    $n = $numbers[0];
+    for ($i = 0; $i < count($numbers); $i++) {
+        if (isset($numbers[$i+1])) {
+            $n = $n * $numbers[$i+1];
+        }
+    }
+    return $n;
+}
